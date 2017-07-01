f1151e1b0f2793c5bd85f827072ee94e30b216f9
Juanito Fatas <juanito-fatas@cookpad.jp>
2017-07-01 18:09:42 +0900

    Write a spec for sign up service

---
+++ spec/services/sign_up_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe SignUp do
+  it "creates an user" do
+    user_params = { "email" => "matt@example.com", "password" => "password" }
+    request = double(env: { "repo" => repo })
+
+    SignUp.new(user_params, request).run
+
+    expect(repo.all(User).last).to include(email: "matt@example.com")
+  end
+
+  def repo
+    Kingfisher::Repo.new(Kingfisher::DatabaseBackends::SqlLite3.new)
+  end
+end
